3a3411fc23f4de09c0bfb7c46a6f5999a2a05488
Brett Wagner <wagnerb@udel.edu>
2021-10-05 13:39:35 -0400

    Basic Story Selection Structure for #13

--- edu-storybook/src/pages/StorySelection.jsx
+++ edu-storybook/src/pages/StorySelection.jsx
@@ -4,12 +4,33 @@ import React, {useState} from "react";
  * 
  * @returns 
  */
+
+function Book(props) {
+  return(
+    <div>
+      <div>
+        {props.title}
+      </div>
+      <div>
+        <img src={props.image}></img>
+      </div>
+      <div>
+        {props.description}
+      </div>
+    </div>
+  )
+}
+
 export default function StorySelection(){
 
   return(
-      <div>
-          <button> </button>
-      </div>
+    <div>
+      {/*Navigation Bar Here*/}
+      <h1>Story Selection</h1>
+      <h2>Choose a Story to Read:</h2>
+      <div>{/*Buffer Space*/}</div>
+      {/*Add Books Here*/}
+    </div>
   )
 
 }
