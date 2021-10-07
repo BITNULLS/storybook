ca4682afcdff40b0a2c55ed529f220ea2120a4ce
Viva Laas <vivalaas@udel.edu>
2021-10-06 20:43:39 -0400

    Created a rough draft of the login and register pages, still need connecting links for pages to get into

--- edu-storybook/src/pages/login-register/Login.jsx
+++ edu-storybook/src/pages/login-register/Login.jsx
@@ -1,19 +1,51 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 
 /** This is the Login Page
  *  This is an example of a class for React
  */
-class Login extends React.Component {
-    render() {
-      return(
-        <div>
-          <button/>
-          <p> Hello World! </p>
+ class Login extends React.Component {
+  render() {
+    return(
+      <div class="row mt-5">
+      <div class="col-12 col-md-4 col-sm-3"></div>
+      <div class="col">
+        <h1 class="text-center mb-5">Login</h1>
+
+        <div class="card pl-1 pr-2">
+          <div class="card-body">
+            <form>
+
+              <div class="form-group mb-4">
+                <label for="username">Username:</label>
+                <input type="text" formControlName="username" class="form-control"></input>
+              </div>
+
+              <div class="form-group mb-4">
+                <label for="password">Password:</label>
+                <input type="password" formControlName="password" class="form-control"></input>
+              </div>
+
+              <div class="form-group">
+                <button class="btn btn-primary">Sign In</button >
+              </div>
+
+            </form>
+
+          </div >
+
         </div>
-      )
-      
-    }
+
+        <div class="text-center mt-5">
+          <a routerLink="/Register" class="btn btn-link">Sign In</a>
+          <a routerLink="/ForgotPassword" class="btn btn-link">Forgot Password</a>
+        </div>
+      </div >
+      <div class="col-12 col-md-4 col-sm-3"></div>
+    </div >
+    );
+    
+  }
 }
 
 export default Login;
