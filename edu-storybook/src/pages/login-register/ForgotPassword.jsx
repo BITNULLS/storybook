5e9437bb66741c04787cc4c56da84548baf3db8f
luckyruby1 <lisapathania@yahoo.com>
2021-10-06 20:53:58 -0400

    Created basic UI of Forgot & Reset pages

--- edu-storybook/src/pages/login-register/ForgotPassword.jsx
+++ edu-storybook/src/pages/login-register/ForgotPassword.jsx
@@ -1,15 +1,44 @@
-import React, {useState} from "react";
+import React, {Component} from "react";
 
 /**
  * 
- * @returns 
+ * Displays the 'username' and 'email address' fields with 'send email' button
+ * Add backend: searches for a matching username (hit/miss) first and then sends email
+ * Add: if invalid username/email, error message display
+ *
  */
-function ForgotPassword(){
+class ForgotPassword extends React.Component {
 
-  return(
-      <div>
-          <button> </button>
-      </div>
-  )
+  render() {
+    return (
+      <div class="row mt-5">
+        <div class="col"></div>
+        <div class="col">
+          <h1 class="text-center mb-5">Forgot Your Password? </h1>
+          <h4 class="text-center mb-5"> Don't worry! We will send you an email to update your password. </h4>
+  
+              <form>
+                <div class="form-group mb-4">
+                  <label for="username">Username:</label>
+                  <input type="text" formControlName="username" class="form-control"></input>
+                </div>
+                <div class="form-group mb-4">
+                  <label for="password">Email Address:</label>
+                  <input type="password" formControlName="password" class="form-control"></input>
+                </div>
+                <div class="text-center form-group">
+                  <button class="btn btn-primary">Send Email</button >
+                </div>
+              </form>
 
+          <div class="text-center mt-5">
+            <a routerLink="/login" class="btn btn-link">Back to Sign In</a>
+          </div>
+        </div >
+        <div class="col"></div>
+      </div >
+    );
+  }
 }
+
+export default ForgotPassword;
--- edu-storybook/src/pages/login-register/ResetPassword.jsx
+++ edu-storybook/src/pages/login-register/ResetPassword.jsx
@@ -1,15 +1,41 @@
-import React, {useState} from "react";
+import React, {Component} from "react";
 
 /**
  * 
- * @returns 
+ * Creates a new password if valid credentials are found from ForgotPassword page
+ * Add backend: updates the old password stored in database
+ * 
  */
-function ResetPassword(){
 
-  return(
-      <div>
-          <button> </button>
-      </div>
-  )
+ class  ResetPassword extends React.Component{
 
+ render() {
+    return (
+      <div class="row mt-5">
+        <div class="col"></div>
+        <div class="col">
+          <h1 class="text-center mb-5"> Reset Password </h1>  
+              <form>
+                <div class="form-group mb-4">
+                  <label for="username">New Password:</label>
+                  <input type="text" formControlName="username" class="form-control"></input>
+                </div>
+                <div class="form-group mb-4">
+                  <label for="password">Confirm Password:</label>
+                  <input type="password" formControlName="password" class="form-control"></input>
+                  <p class="mb-5"> Password must be at least eight characters </p>  
+                </div>
+                <div class="text-center form-group">
+                  <button class="btn btn-primary">Confirm</button >
+                </div>
+              </form>
+
+          <div class="text-center mt-5">
+          </div>
+        </div >
+        <div class="col"></div>
+      </div >
+    );
+  }
 }
+export default ResetPassword;
