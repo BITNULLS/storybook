0c1481fca79b4b3d130e7b6c163f330ae7b932eb
James Villemarette <jvill@udel.edu>
2021-10-07 14:25:23 -0400

    #17: stub work on backend doc and api

--- backend/server.py
+++ backend/server.py
@@ -3,5 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return {
+        "status": "ok"
+    }
+
+@app.route("/login")
+def login():
+    return None 
