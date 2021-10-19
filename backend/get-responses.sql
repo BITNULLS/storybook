acdad378e88870ad6101f3a2808503e66bfd1ce6
Kara Pelster <42856946+kpelster@users.noreply.github.com>
2021-10-19 12:52:23 -0400

    draft queries for users, responses, and actions

---
+++ backend/get-responses.sql
@@ -0,0 +1,13 @@
+/* select query to get user response */
+
+select user_profile.username, study.study_name, book.book_name, question.question, answer.answer, user_response.answered_on from user_profile
+
+inner join study on user_profile.study_id = study.study_id
+
+inner join book on study.study_id = book.book_id
+
+inner join user_response on user_profile.user_id = user_response.user_id
+
+inner join answer on user_response.answer_id = answer.answer_id
+
+inner join question on answer.question_id = question.question_id
