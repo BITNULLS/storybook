d96f26a8ca15b122a21ef0c64c050d871ccc1296
Kara Pelster <42856946+kpelster@users.noreply.github.com>
2021-10-19 14:50:43 -0400

    changed action.session_id to action.occurred_on

--- backend/get-actions.sql
+++ backend/get-actions.sql
@@ -1,6 +1,6 @@
 /* select query to get user actions */
 
-select user_profile.username,action.current_page, action.prev_page, action.link, action.session_id from user_profile
+select user_profile.username,action.current_page, action.prev_page, action.link, action.occurred_on from user_profile
 
 inner join action on user_profile.user_id = action.user_id;
 
