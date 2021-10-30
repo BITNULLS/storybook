eb7a5660b6bf55d5598a0bea0e3eab3b86b314a8
Viva Laas <vivalaas@udel.edu>
2021-10-30 15:27:13 -0400

    changed navbar to collapse as per issue #63

--- edu-storybook/src/App.js
+++ edu-storybook/src/App.js
@@ -39,25 +39,6 @@ function App() {
       </div>
     </nav>
 
-      <nav  class="navbar navbar-expand-lg navbar-light bg-light">
-        <li class="nav-item">
-          <Link class="navbar-brand" to="/">EduStorybook </Link>
-        </li>      
-
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <Link class="nav-link" to="/">Home </Link>
-          </li>
-          <li class="nav-item">
-            <Link class="nav-link" to="/Login">Login</Link>
-          </li>
-          <li class="nav-item">
-            <Link class="nav-link disabled" to="/StorySelection">Story Selection</Link>
-          </li>
-        </ul>
-       
-      </nav>
-
         <Switch>
           <Route exact path="/">
             <HomePage />
