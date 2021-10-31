5db9e46eba4fdb688a305b7635a4b5297878d142
luckyruby1 <lisapathania@yahoo.com>
2021-10-31 11:22:08 -0400

    added NavBar class

--- edu-storybook/src/NavBar.js
+++ edu-storybook/src/NavBar.js
@@ -2,33 +2,68 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 
 class NavBar extends Component {
+    constructor() {
+      super()
+  
+      this.state = {
+        loggedIn: false
+      }      
+    }
+
+    /* componentDidMount() {
+      if (document.cookie.split(';').filter((item) => item.trim().startsWith('loggedIn=')).length) {
+        this.setState({ loggedIn: true })
+      }
+      window.setInterval(() => {
+        if (document.cookie.split(';').filter((item) => item.trim().startsWith('loggedIn=')).length) {
+          this.setState({ loggedIn: true })
+        }
+        else {
+          this.setState({ loggedIn: false })
+        }
+      }, 500)
+    } */
 
 render() {
   return (
+    
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
         <li class="nav-item">
           <Link class="navbar-brand" to="/">EduStorybook </Link>
         </li>      
 
-        <ul class="navbar-nav mr-auto">
-        
-        <li class="nav-item active">
+        <li class="nav-item">
           <Link class="nav-link" to="/">Home </Link>
         </li>
 
-        <li class="nav-item">
+        <ul class="navbar-nav mr-auto">
+        {this.state.loggedIn ?
+                  <React.Fragment>
+                    <Link className="nav-link mr-3" to="/myaccount">My account</Link>
+                    <Link className='nav-link' to='/'>Log out</Link>
+                    <Link className='nav-link' to="/StorySelection">Story Selection</Link>
+                  </React.Fragment>
+                  :
+                  <React.Fragment>
+                    <Link className='nav-link' to='/Login'>Log in</Link>
+                    <Link className='nav-link' to='/Register'>Register</Link>
+                  </React.Fragment>
+                }
+
+     { /*  <li class="nav-item">
           <Link class="nav-link" to="/Login">Login</Link>
-        </li>
+        </li> 
 
         <li class="nav-item">
           <Link class="nav-link disabled" to="/StorySelection">Story Selection</Link>
-        </li>
+              </li> */}
 
         </ul>
 
       </nav>
+      
   );
+  
 }
 }
 export default NavBar;
--- edu-storybook/src/pages/login-register/Login.jsx
+++ edu-storybook/src/pages/login-register/Login.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 /** This is the Login Page
