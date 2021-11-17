4817a2e62d146e06ffed0af0c8df6e493f5266c8
luckyruby1 <lisapathania@yahoo.com>
2021-11-16 20:22:17 -0500

    afixed spacing of send_email

--- backend/server.py
+++ backend/server.py
@@ -664,7 +664,8 @@ def register():
         }
     
     send_email(first_name + last_name, email, 'Edu Storybooks', 'edustorybooks@gmail.com', 
-        'Welcome to Edu Storybooks', 'Dear ' + first_name + ' ' + last_name + ',' + '\n\nThanks for registering an account with Edu Storybooks! :)')
+        'Welcome to Edu Storybooks', 'Dear ' + first_name + ' ' + last_name + ',' + 
+        '\n\nThanks for registering an account with Edu Storybooks! :)')
 
     return {
         "status": "ok"
