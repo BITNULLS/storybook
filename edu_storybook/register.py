6af4d4515a07bf75668b225b090320683f172191
littletinkerbelle7@gmail.com <lisapathania@yahoo.com>
2022-03-08 13:22:22 -0500

    fixing register navbar and adding spacing comments

--- edu_storybook/register.py
+++ edu_storybook/register.py
@@ -3,6 +3,8 @@ register.py
     This lets a user register for the website.
 
 Routes:
+
+
     /register
 """
 
@@ -10,14 +12,22 @@ from flask import request
 from flask import Blueprint
 
 from templates import TEMPLATES
+from navbar import make_navbar
 
 register = Blueprint('register', __name__)
 
 @register.route("/register")
 def gen_register():
+    
+    auth = None
+    if 'Authorization' in request.cookies:
+        auth = request.cookies['Authorization']
+
     register_page = TEMPLATES["_base"].substitute(
         title = "Register an Account",
         description = "Make an account with our website",
-        body = TEMPLATES["register"].substitute()
+        body = TEMPLATES["register"].substitute(
+             navbar = make_navbar( auth )
+        )
     )
     return register_page
