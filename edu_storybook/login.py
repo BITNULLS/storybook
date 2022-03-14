4fa2b945abfd0ed2905af006649642e111193524
Raj Trivedi <triudrav@udel.edu>
2022-03-14 13:04:27 -0400

    Merged Lisa's PR into my local branch

--- edu_storybook/login.py
+++ edu_storybook/login.py
@@ -1,7 +1,8 @@
 """
 login.py
-    
+
 Routes:
+
     /login
 """
 
@@ -9,15 +10,22 @@ from flask import request
 from flask import Blueprint
 
 from templates import TEMPLATES
+from navbar import make_navbar
 
 login = Blueprint('login', __name__)
 
 @login.route("/login")
 def gen_login():
+    
+    auth = None
+    if 'Authorization' in request.cookies:
+        auth = request.cookies['Authorization']
+
     login_page = TEMPLATES["_base"].substitute(
         title = "Admin: Book Manager",
         description = "A motivational storybook that helps students learn.",
-        body = TEMPLATES["login"].substitute()
+        body = TEMPLATES["login"].substitute(
+            navbar = make_navbar( auth )
+        )
     )
     return login_page
-
