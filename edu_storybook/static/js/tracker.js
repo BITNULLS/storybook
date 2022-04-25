06c744f14c4366e7c2b8fa410843a5d12cc637f1
Lisa Pathania <lisapathania@wifi-roaming-128-4-144-123.wifi.udel.edu>
2022-04-25 16:14:48 -0400

    updated tracker.js

--- edu_storybook/static/js/tracker.js
+++ edu_storybook/static/js/tracker.js
@@ -35,12 +35,13 @@ function calculateTime() {
 
 var lastAction = calculateTime();
 
-function trackEvent(actionStart, actionStop, actionID, description) {
+function trackEvent(book_id, actionStart, actionStop, actionID, description) {
 
     // error with open_book() so hardcoded book_id
     // var book_id = $('#book_id').val();
-    var book_id = 122;
-    $.ajax( {
+   // var book_id = 122;
+    
+   $.ajax( {
         type: "POST",
         url: "/api/storyboard/action",
         data: {
@@ -60,90 +61,99 @@ function trackEvent(actionStart, actionStop, actionID, description) {
     }); 
 }
 
-function open_book() {
-    var actionTime = calculateTime();    
-//  console.log("User opened book");
-    trackEvent(lastAction, actionTime, 0, 'User opened the book from the book dashboard.');
+function open_book(book_id) {
+    var actionTime = calculateTime();  
+ // const book_id = document.getElementById("book_id").value;
+ // console.log("Book_id=" + book_id);
+  
+    console.log("User opened book at book_id:" + book_id);
+    trackEvent(book_id, lastAction, actionTime, 0, 'User opened the book from the book dashboard.');
     lastAction = actionTime;
 }
 
-function close_book() {
+function close_book(book_id) {
     var actionTime = calculateTime();
-//    console.log("User closed book");
-    trackEvent(lastAction, actionTime, 1, 'User exited the book back to some other page.');
+   // console.log("User closed book at book_id:" + book_id);
+    
+    document.getElementById("edustorybook-user-link")
+    document.getElementById("story-selection-link")
+    document.getElementById("logout-button")
+
+    trackEvent(book_id, lastAction, actionTime, 1, 'User exited the book back to some other page.');
     lastAction = actionTime;
 }
 
 // ! fixed for png images for now
-function click_page() {
+function click_page(book_id, page_num) {
     var actionTime = calculateTime();
- //   console.log("Click on page");
-    trackEvent(lastAction, actionTime, 2, 'User clicked somewhere on the page (not a link, forward or backwards, textbox, etc).');
+    console.log("Click on page where book_id:" + book_id + "and page_num:" + page_num);
+    trackEvent(book_id, lastAction, actionTime, 2, 'User clicked somewhere on the page (not a link, forward or backwards, textbox, etc).');
     lastAction = actionTime;
 }
 
 // ? wait until all pages have accessible links
-function click_link() {
+function click_link(book_id, page_num) {
     var actionTime = calculateTime();
 //  trackEvent(lastAction, actionTime, 3, "User clicked a link.");
 } 
 
-function exit_mouse_page() {
+function exit_mouse_page(book_id, page_num) {
     document.addEventListener("mouseleave", (event) => {
     var actionTime = calculateTime();
     
     if (event.clientY <= 0 || event.clientX <= 0 || (event.clientX >= window.innerWidth || event.clientY >= window.innerHeight)) {
    //   console.log("I'm out");
-      trackEvent(lastAction, actionTime, 4, 'Mouse of user left the webpage.')
+      trackEvent(book_id, lastAction, actionTime, 4, 'Mouse of user left the webpage.')
       lastAction = actionTime;
       }
    });
 }
 
-function enter_mouse_page() {
+function enter_mouse_page(book_id, page_num) {
     document.addEventListener("mouseenter", (event) => {
     var actionTime = calculateTime();
 
     if ((event.clientY > 0 && event.clientY < window.innerHeight) && (event.clientX > 0 && event.clientX < window.innerWidth)) {
      //   console.log("I'm in");
-        trackEvent(lastAction, actionTime, 5, 'Mouse of user re-entered the webpage.')
+        trackEvent(book_id, lastAction, actionTime, 5, 'Mouse of user re-entered the webpage.')
         lastAction = actionTime;
        }
     });
 }
 
-function exit_tab() {
+function exit_tab(book_id) {
     document.addEventListener("visibilitychange", () => {
         var actionTime = calculateTime();
         if (document.visibilityState != "visible") {
-      //      console.log("Tab is inactive");
-            trackEvent(lastAction, actionTime, 6, 'User switched to another tab.');
+            console.log("Tab is inactive");
+            trackEvent(book_id, lastAction, actionTime, 6, 'User switched to another tab.');
             lastAction = actionTime;
         }
     });
 }
 
-function enter_tab() {
+function enter_tab(book_id) {
     document.addEventListener("visibilitychange", () => {
         var actionTime = calculateTime();
         if (document.visibilityState == "visible") {
-   //         console.log("Tab is active");
-            trackEvent(lastAction, actionTime, 7, 'User switched back to our webpage tab.');
+            console.log("Tab is active");
+            trackEvent(book_id, lastAction, actionTime, 7, 'User switched back to our webpage tab.');
             lastAction = actionTime;
         }
     });
 }
 
-function turn_page_forward() {
+function turn_page_forward(book_id, page_num) {
     var actionTime = calculateTime();
-    trackEvent(lastAction, actionTime, 10, 'User turned forward a page.');
+//    console.log("Print the next page");
+    trackEvent(book_id, lastAction, actionTime, 10, 'User turned forward a page.');
     lastAction = actionTime;
 }
 
-function turn_page_backward() {    
+function turn_page_backward(book_id, page_num) {    
     var actionTime = calculateTime();
-  //  console.log("User turned page backward at " + actionTime);
-    trackEvent(lastAction, actionTime, 11, 'User turned backward a page.');
+ //   console.log("User turned page backward at " + actionTime);
+    trackEvent(book_id, lastAction, actionTime, 11, 'User turned backward a page.');
     lastAction = actionTime;
 }
 
@@ -161,7 +171,5 @@ function exit_text_response() {
 //    trackEvent(lastAction, dateString, 13, '');
 }
 
-//enter_tab();
-//exit_tab();
 //enter_mouse_page();
 //exit_mouse_page();
